4935dde888723bb1025e48c0bb2de085bd525d2f
Simon Castle <simon.c.castle@gmail.com>
2025-08-26 08:11:34 +0100

    Add support for a few location setups and more tooltip data

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -212,14 +212,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "c5b7d464bb719f744d97205867f0e8f4
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "8a42f44e52258966a0d4d21dca16a489",
+"flutter_bootstrap.js": "48ec2e17924dcf4f692330e933f41fbb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "bfd84af22c6f8a36fbbc0d601a24c33c",
 "/": "bfd84af22c6f8a36fbbc0d601a24c33c",
-"main.dart.js": "bc8457c8e0188051acfe565a7c567d62",
+"main.dart.js": "6c2c52f4eba4312bde4ca4697312e350",
 "manifest.json": "8c7fe0e7c7c88c79157216b994ccf705",
 "version.json": "c823c30f55884a92c136ccc8b7106ef7"};
 // The application shell files that are downloaded before a service worker can
